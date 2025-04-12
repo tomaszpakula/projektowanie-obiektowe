695698f67e2fed28d63107574541ab0bf41f63ed
tomaszpakula <tomasz.pakula11@gmail.com>
2025-04-12 20:33:01 +0200

    demo zadanie2

--- zadanie2/productTest.sh
+++ zadanie2/productTest.sh
@@ -1,25 +1,25 @@
 echo -e "Get all products\n"
-curl -s -X GET http://localhost:8080/products | jq
+curl -s -X GET http://localhost:8000/products | jq
 
 echo -e "Get product by id (product exist)\n"
-curl -s -X GET http://localhost:8080/products/1 | jq
+curl -s -X GET http://localhost:8000/products/1 | jq
 
 echo -e "Get product by id (product doesn't exist)\n"
-curl -s -X GET http://localhost:8080/products/10 | jq
+curl -s -X GET http://localhost:8000/products/10 | jq
 
 echo -e "Create new product\n"
-curl -s -X POST http://localhost:8080/products \
+curl -s -X POST http://localhost:8000/products \
   -H "Content-Type: application/json" \
   -d '{"name":"komputer", "price": 3000}' | jq
 
 echo -e "Update product with id 2\n"
-curl -s -X PUT http://localhost:8080/products/2 -H "Content-Type: application/json" -d '{"name":"komputer", "price" : 6000}' | jq
+curl -s -X PUT http://localhost:8000/products/2 -H "Content-Type: application/json" -d '{"name":"komputer", "price" : 6000}' | jq
 
 echo -e "Get all products\n"
-curl -s -X GET http://localhost:8080/products | jq
+curl -s -X GET http://localhost:8000/products | jq
 
 echo -e "Delete product with id 2\n"
-curl -s -X DELETE http://localhost:8080/products/2 | jq
+curl -s -X DELETE http://localhost:8000/products/2 | jq
 
 echo -e "Get all products\n"
-curl -s -X GET http://localhost:8080/products | jq
+curl -s -X GET http://localhost:8000/products | jq
