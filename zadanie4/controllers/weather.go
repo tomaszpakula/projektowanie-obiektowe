edd62247fcadde1a5283ef766d293c62fb7d3e4f
tomaszpakula <tomasz.pakula11@gmail.com>
2025-04-12 16:31:38 +0200

    zadanie 4 wymaganie 5

--- zadanie4/controllers/weather.go
+++ zadanie4/controllers/weather.go
@@ -3,26 +3,40 @@ package controllers
 import (
 	"github.com/labstack/echo/v4"
 	"net/http"
+	"strings"
 	"zadanie4/database"
 	"zadanie4/models"
 	"zadanie4/services"
 )
 
 func GetWeather(c echo.Context) error {
-	city := c.QueryParam("city")
-	weatherProxy := services.CreateWeatherProxy()
-	weather, err := weatherProxy.FetchWeather(city)
-	if err == nil {
-		if err := database.DB.Create(weather).Error; err != nil {
-			return c.JSON(http.StatusInternalServerError, echo.Map{"error": "Could not save weather data"})
+	cities := strings.Split(c.QueryParam("city"), ",")
+
+	var weatherList []models.Weather
+
+	for _, city := range cities {
+		weatherProxy := services.CreateWeatherProxy()
+		weather, err := weatherProxy.FetchWeather(city)
+		if err == nil {
+			if err := database.DB.Create(weather).Error; err != nil {
+				return c.JSON(http.StatusInternalServerError, echo.Map{"error": "Could not save weather data"})
+			}
+			weatherList = append(weatherList, *weather)
+		} else {
+			var weatherFromDB models.Weather
+			result := database.DB.Where("location = ?", city).Last(&weatherFromDB)
+			if result.Error != nil {
+				weatherList = append(weatherList, models.Weather{
+					Location:  city,
+					TempC:     0,
+					TempF:     0,
+					Condition: "Not found",
+				})
+			} else {
+				weatherList = append(weatherList, weatherFromDB)
+			}
 		}
-		return c.JSON(http.StatusOK, weather)
 	}
 
-	var weatherFromDB models.Weather
-	result := database.DB.Where("location = ?", city).Last(&weatherFromDB)
-	if result.Error != nil {
-		return c.JSON(http.StatusNotFound, echo.Map{"error": "City not found"})
-	}
-	return c.JSON(http.StatusOK, weatherFromDB)
+	return c.JSON(http.StatusOK, weatherList)
 }
